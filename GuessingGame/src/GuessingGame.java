513c5aee1447b54e2f95caec31aaf975ae0387cb
zizoumunoz <zmunoz@outlook.es>
2020-10-02 23:03:11 -0700

    Added empty window.

--- GuessingGame/src/GuessingGame.java
+++ GuessingGame/src/GuessingGame.java
@@ -1,9 +1,12 @@
 import javax.swing.JFrame;
 
 public class GuessingGame extends JFrame {
+	public GuessingGame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("Zinedine's Guessing Game");
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
