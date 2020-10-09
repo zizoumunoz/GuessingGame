65bdcb65b3cf81a666923b7da82380375aaa9efc
zizoumunoz <zmunoz@outlook.es>
2020-10-09 10:58:26 -0700

    Added JLabel with title

--- GuessingGame/src/GuessingGame.java
+++ GuessingGame/src/GuessingGame.java
@@ -1,13 +1,18 @@
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class GuessingGame extends JFrame {
 	public GuessingGame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTitle("Zinedine's Guessing Game");
+		getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Zinedine's Guessing Game");
+		lblNewLabel.setBounds(217, 11, 47, 14);
+		getContentPane().add(lblNewLabel);
 	}
 
 	public static void main(String[] args) {
 
 	}
-
 }
